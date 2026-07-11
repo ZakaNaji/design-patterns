5dd8a7402821e431840aa2b7907f558c3d0ea0f8
ZakaNaji <zakaria.naji@edu.uca.ma>
2026-07-11 12:27:20 +0100

    Composite: demo of the EquipmentCOmponent Composite

--- src/main/java/com/znaji/composite/OutdoorStoreDemo.java
+++ src/main/java/com/znaji/composite/OutdoorStoreDemo.java
@@ -4,14 +4,60 @@ import java.math.BigDecimal;
 public class OutdoorStoreDemo {
 
     public static void main(String[] args) {
-        // empty kit example:
-        EquipmentKit emptyKit = new EquipmentKit("Empty Kit");
+        Product fireStarter = new Product(
+                "Fire Starter",
+                new BigDecimal("120.00"),
+                100
+        );
 
-        // price and weight of the empty kit
-        BigDecimal emptyKitPrice = emptyKit.price();
-        int emptyKitWeight = emptyKit.weightInGrams();
+        Product waterFilter = new Product(
+                "Water Filter",
+                new BigDecimal("300.00"),
+                250
+        );
 
-        System.out.println("Empty Kit Price: " + emptyKitPrice);
-        System.out.println("Empty Kit Weight: " + emptyKitWeight + "g");
+        Product tarp = new Product(
+                "Tarp",
+                new BigDecimal("400.00"),
+                900
+        );
+
+        Product campingPot = new Product(
+                "Camping Pot",
+                new BigDecimal("230.00"),
+                500
+        );
+
+        Product portableStove = new Product(
+                "Portable Stove",
+                new BigDecimal("450.00"),
+                800
+        );
+
+        EquipmentKit cookingKit =
+                new EquipmentKit("Cooking Kit");
+
+        cookingKit.add(campingPot);
+        cookingKit.add(portableStove);
+
+        EquipmentKit weekendPack =
+                new EquipmentKit("Weekend Survival Pack");
+
+        weekendPack.add(fireStarter);
+        weekendPack.add(waterFilter);
+        weekendPack.add(tarp);
+        weekendPack.add(cookingKit);
+
+        weekendPack.print("");
+
+        System.out.println();
+        System.out.println(
+                "Total price: " + weekendPack.price()
+        );
+        System.out.println(
+                "Total weight: "
+                        + weekendPack.weightInGrams()
+                        + "g"
+        );
     }
 }
